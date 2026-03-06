cf1c66a867156c2ad58ebfe797ada74f312cdc44
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-06 17:17:02 +0100

    feat: initiated cookie-parser and swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('Alter')
+    .setDescription('Alter API Description')
+    .setVersion('1.0')
+    .build();
+
   const app = await NestFactory.create(AppModule);
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.enableCors();
 }
 bootstrap();
