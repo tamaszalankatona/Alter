d73ea5e992e764ea2944802d235925478d25bfb9
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-10 14:27:58 +0100

    feat: added UsersService, PrismaService

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [AuthService, UsersService, PrismaService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
