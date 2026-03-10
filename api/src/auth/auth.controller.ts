7c18512879caffad235ab1289d103d3fb172d46c
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-10 14:27:29 +0100

    feat: created signup endpoint

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+} from '@nestjs/swagger';
+import { User } from 'generated/prisma/client';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UserResponseDto } from 'src/users/dto/user-response.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(201)
+  @ApiCreatedResponse({
+    description: 'User created successfully',
+    type: UserResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description:
+      'Request body does not meet requirements (e.g. invalid email format, password too short)',
+  })
+  @ApiBody({ type: CreateUserDto })
+  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.createUser(createUserDto);
+  }
+}
