b8a191b47e9a90f7bcf0efd25aa004c708323f59
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-10 14:21:41 +0100

    feat: added configModule, PrismaService

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
