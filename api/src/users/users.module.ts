c4f178cefb35eaad54ee91a992d8ded74efe28d4
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-10 14:28:17 +0100

    feat: added PrismaService

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService, PrismaService],
 })
 export class UsersModule {}
