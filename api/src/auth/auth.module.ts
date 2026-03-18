1297e38dc57562efd4576cbb40701c2d5ae74bdd
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-18 09:00:17 +0100

    feat(auth): configure JwtModule with secret and expiration

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from 'src/users/users.service';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService, UsersService, PrismaService],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      secretOrPrivateKey: process.env.JWT_SECRET,
+
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  providers: [AuthService, UsersService, PrismaService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
