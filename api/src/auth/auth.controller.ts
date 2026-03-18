fafa0be579d452470bc170c9297b1fefe38fd1af
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-18 08:59:38 +0100

    feat(auth): create sign-in endpoint, inject AuthService, return 201 status on signup

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,30 +1,75 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import {
-  ApiBadRequestResponse,
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import {
   ApiBody,
+  ApiConflictResponse,
   ApiCreatedResponse,
+  ApiOkResponse,
 } from '@nestjs/swagger';
 import { User } from 'generated/prisma/client';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UserResponseDto } from 'src/users/dto/user-response.dto';
 import { UsersService } from 'src/users/users.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('signup')
-  @HttpCode(201)
+  @HttpCode(HttpStatus.CREATED)
   @ApiCreatedResponse({
     description: 'User created successfully',
     type: UserResponseDto,
   })
-  @ApiBadRequestResponse({
+  @ApiConflictResponse({
     description:
       'Request body does not meet requirements (e.g. invalid email format, password too short)',
   })
   @ApiBody({ type: CreateUserDto })
-  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.usersService.createUser(createUserDto);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'User signed in successfully',
+    example: 'Sign in successfull',
+  })
+  @ApiConflictResponse({
+    description:
+      'Request body does not meet requirements (e.g. invalid email format, password too short)',
+  })
+  @ApiBody({
+    type: SignInDto,
+  })
+  async signIn(
+    @Body() signInDto: SignInDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const user = await this.authService.validateUser(signInDto);
+
+    const token = await this.authService.signIn(user);
+
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    return { message: 'Sign in successfull' };
   }
 }
