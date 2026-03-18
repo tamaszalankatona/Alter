c83f63aa2c68c876dd6dec1fcb059cecc4a340e9
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-18 09:01:02 +0100

    implement user validation and JWT login

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { comparePassword } from 'src/utils/password-hashing.utils';
+import { User } from 'generated/prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(signInDto: SignInDto): Promise<any> {
+    const user = await this.usersService.findUserByEmail(signInDto.email);
+
+    if (user && (await comparePassword(signInDto.password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    throw new UnauthorizedException('User with provided cretentials not found');
+  }
+
+  async signIn(user: User): Promise<Record<string, string>> {
+    const payload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    // return token
+    return {
+      access_token: await this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_SECRET,
+      }),
+    };
+  }
+}
