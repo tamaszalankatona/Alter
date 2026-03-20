a4566025c6bde187d1ab02bc05d30b1f95dbd682
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-20 17:22:27 +0100

    chore(users): imported AuthModule to UsersModule

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma.service';
 import { UsersController } from './users.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [UsersService, PrismaService],
   controllers: [UsersController],
 })
