5f789ec3cf1b4fd99f0f76443d2ccb881c2fe105
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-03-20 16:57:04 +0100

    chore: cookieParser, enableCors moved to the top of the main function

--- api/src/main.ts
+++ api/src/main.ts
@@ -12,11 +12,12 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  app.use(cookieParser());
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
-  app.use(cookieParser());
-  app.enableCors();
 }
 bootstrap();
